d1f7a2a45a07e92db8006f4fb341dfb99674f72f
Muhammad <mnh78614@gmail.com>
2016-11-21 18:42:06 -0800

    Added Work section.

--- src/Work.js
+++ src/Work.js
@@ -3,13 +3,37 @@ import React, { Component } from 'react';
 import {Row, Col} from 'react-grid-system';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
+const styles = {
+	image: {
+		maxWidth: 400,
+		maxHeight: 400,
+		display: 'block',
+		marginLeft: 'auto',
+		marginRight: 'auto'
+	}
+}
 
 class Work extends Component {
   render() {
     return (
-	<Row>
-		Placeholder
-	</Row>
+    	<div>
+          <Card>
+            <CardMedia style={styles.image}
+              overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+            
+            >
+              <img src="http://a4.files.biography.com/image/upload/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE4MDAzNDEwMDU4NTc3NDIy.jpg"
+              	alt=''
+              	/>
+            </CardMedia>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+          </Card>
+          </div>
     );
   }
 }
